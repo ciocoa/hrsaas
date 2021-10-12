8beabbe266838cac77757689e8b5864d20a12e47
RedsNina <acgsx@live.com>
2021-10-12 14:15:58 +0800

    基于模版进行项目初始化

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  semi: false,
+  htmlWhitespaceSensitivity: 'ignore'
+}

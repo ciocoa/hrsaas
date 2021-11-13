70e46d5716be962d25d74a185f90b1e97acc81df
clarayame <acgsx@live.com>
2021-11-13 09:05:36 +0800

    update .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,4 @@
-//https://blog.csdn.net/Sheng_zhenzhen/article/details/108685176
-//
+// https://blog.csdn.net/Sheng_zhenzhen/article/details/108685176
 module.exports = {
   root: true,
   env: {
@@ -16,25 +15,25 @@ module.exports = {
     defineProps: true,
     defineExpose: true
   },
-  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/prettier'],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
   parserOptions: {
     ecmaVersion: 2021
   },
   rules: {
     'no-unused-vars': 'off',
+    'no-console': 'off',
+    'no-plusplus': 'off',
+    'no-useless-escape': 'off',
+    'no-bitwise': 'off',
+    'no-param-reassign': 'off',
     'linebreak-style': ['error', 'unix'],
     'import/no-unresolved': 'off',
     'import/extensions': 'off',
     'import/no-absolute-path': 'off',
     'import/no-extraneous-dependencies': 'off',
-    'vue/no-multiple-template-root': 'off',
     'vue/html-self-closing': 'off',
-    'no-console': 'off',
-    'no-plusplus': 'off',
-    'no-useless-escape': 'off',
-    'no-bitwise': 'off',
-    'vue/no-setup-props-destructure': 'off',
-    'no-param-reassign': 'off'
+    'vue/no-multiple-template-root': 'off',
+    'vue/no-setup-props-destructure': 'off'
   },
   overrides: [
     {
